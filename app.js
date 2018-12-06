edf00f9722aa3f9a71a3f184d891f82002a34764
ahd3r <ander11110@gmail.com>
2018-12-06 21:02:08 +0200

    Done It

--- app.js
+++ app.js
@@ -7,12 +7,17 @@ weather.getWeather().then(data=>{
 }).catch(err=>console.log(err)); // It shoultn't being in page
 document.querySelector('#btn-change-loc').addEventListener('click', function(){
   if(document.querySelector('#City').value!==''&&document.querySelector('#State').value!==''){
-    storage.setLocalData(document.querySelector('#City').value,document.querySelector('#State').value);
-    dataInStorage=storage.getLocalData();
-    weather=new Weather(dataInStorage.city, dataInStorage.state);
-    weather.getWeather().then(data=>{
-        ui.paint(data);
-    }).catch(err=>console.log(err));
+  	weather=new Weather(document.querySelector('#City').value, document.querySelector('#State').value);
+  	weather.getWeather().then(data=>{
+  		if(data){
+        storage.setLocalData(document.querySelector('#City').value,document.querySelector('#State').value);
+        dataInStorage=storage.getLocalData();
+        weather=new Weather(dataInStorage.city, dataInStorage.state);
+        weather.getWeather().then(data=>{
+            ui.paint(data);
+        }).catch(err=>console.log(err));
+  		}
+  	}).catch(err=>console.log(err));
     $('#exampleModal').modal('hide'); // jquery stuff
   }
 });
--- weather.js
+++ weather.js
@@ -8,7 +8,7 @@ class Weather{
   async getWeather(){
     const response = await fetch(`http://api.wunderground.com/api/${this.APIKey}/conditions/q/${this.state}/${this.city}.json`);
     const responseData=await response.json();
-    return responseData.current_observation;
+  	return responseData.current_observation;
   }
 
   changeLocation(city, state){
